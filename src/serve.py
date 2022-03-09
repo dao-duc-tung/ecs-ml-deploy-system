57f1c198f3499d719104977da5d1ca8af7649f5f
Tung Dao <tungdao17@gmail.com>
2022-03-09 13:48:19 +0800

    Add src

---
+++ src/serve.py
@@ -0,0 +1,25 @@
+import uvicorn
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome!"}
+
+
+@app.post("/api/v1/predict/")
+async def predict(item: Item):
+    return item
+
+
+if __name__ == "__main__":
+    uvicorn.run("serve:app", host="0.0.0.0")
